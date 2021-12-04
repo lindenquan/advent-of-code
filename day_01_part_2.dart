47cbc43b6f1b61d1b025ca286378e60d62ace9d4
Linden Quan <linden.quan@resmed.com>
2021-12-04 10:09:33 -0400

    day 01 part 2

---
+++ day_01_part_2.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+import 'dart:convert';
+
+void main() async {
+  var path = 'day_01.input';
+  var first = -1;
+  var second = -1;
+  var newList = await new File(path)
+      .openRead()
+      .map(utf8.decode)
+      .transform(new LineSplitter())
+      .map(int.parse)
+      .map((element) {
+    if (first == -1) {
+      first = element;
+      return -1;
+    } else if (second == -1) {
+      second = element;
+      return -1;
+    }
+
+    var sum = first + second + element;
+    first = second;
+    second = element;
+    return sum;
+  }).toList();
+
+  var count = 0;
+  var previous = -1;
+  newList.forEach((element) {
+    if (previous != -1 && element > previous) {
+      count++;
+    }
+    previous = element;
+  });
+
+  print(count);
+}
